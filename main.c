5c2f0a8db274ab1215e2e5ad67f5cc7b0a532df4
99002668 <71481601+99002668@users.noreply.github.com>
2020-09-19 10:51:28 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,42 @@
 
 int main()
 {
-	showMenu();
+    printf("\t\t\tWELCOME TO THE PORTAL.");
+    int i;
+    int choice;
 
-	return 0;
+    fp=fopen("library.txt","r+");
+
+    if(fp==NULL)
+    {
+        fp=fopen("library.txt","w");
+        fclose(fp);
+        fp=fopen("library.txt","r+");
+    }
+
+do{
+    printf("\n\t 1:LIST");
+    printf("\n\t 2:ADD");
+    printf("\n\t 3:SEARCH");
+    printf("\n\t 4:MODIFY");
+    printf("\n\t 5:EXIT");
+    printf("\n\t ENTER CHOICE\t");
+    scanf("%d",&i);
+    switch(i)
+    {
+
+    case 1:list();
+           break;
+    case 2:newstudent();
+            break;
+    case 3:search();
+            break;
+    case 4:modify();
+            break;
+    }
+   }
+   while(choice!=5);
+    fclose(fp);
+return 0;
+
 }	
